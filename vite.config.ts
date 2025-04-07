d5178bd685ab9a8cc1de50ee398c3d7de8016f97
faustozamparelli <fausto.zamparelli@gmail.com>
2025-04-07 18:35:19 +0200

    favicon

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
-// vite.config.js
 import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { sveltekit } from '@sveltejs/kit/vite'
 
 export default defineConfig({
-  plugins: [svelte()],
-  base: '/todo/' // Add this line
+  plugins: [sveltekit()]
 })
